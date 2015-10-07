ee645291603c9221be90f3838eb941e8069f7d07
Alejo Fernandez <alejofernandez@gmail.com>
2015-10-07 12:18:47 -0300

    Add controllers model

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express    = require('express');
-var app        = express();
-var bodyParser = require('body-parser');
+var express            = require('express');
+var app                = express();
+var bodyParser         = require('body-parser');
+var BaseJsonController = require('./controllers/BaseJsonController');
+var rootController     = require('./controllers/rootController');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -9,11 +11,10 @@ var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
 // Routes
-router.get('/', function(req, res) {
-  res.json({ message: 'Welcome to Issue Tracker' });
-});
+router.get('/', new BaseJsonController(rootController.welcome).invoke);
 
 // Server
 app.use('/api/v1', router);
 app.listen(port);
+
 console.log('Server started on port ' + port);
